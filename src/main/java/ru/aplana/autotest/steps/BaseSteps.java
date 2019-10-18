8a5e17af6ac9b00d3f02bedb428bb5884be1b4d8
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-18 13:05:50 +0300

    Update BaseSteps.java

--- src/main/java/ru/aplana/autotest/steps/BaseSteps.java
+++ src/main/java/ru/aplana/autotest/steps/BaseSteps.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.aplana.autotest.util.TestProperties;
 
 import java.util.concurrent.TimeUnit;
@@ -17,12 +18,23 @@ public class BaseSteps {
 
         @cucumber.api.java.Before
         public static void init() {
-            System.setProperty("webdriver.chrome.driver", testProperties.getProperty("chromedriver"));
-            driver = new ChromeDriver();
+            switch (testProperties.getProperty("browser")) {
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver", testProperties.getProperty("chromedriver"));
+                    driver = new ChromeDriver();
+                    break;
+         /*   driver.manage().window().maximize();
+            driver.get(testProperties.getProperty("url"));
+            driver .manage().timeouts().implicitlyWait(Integer.parseInt(testProperties.getProperty("timeout.global")), TimeUnit.SECONDS);
+            // driver .manage().timeouts().pageLoadTimeout(Integer.parseInt(testProperties.getProperty("timeout.pageLoad")), TimeUnit.SECONDS); */
+                case "firefox":
+                    System.setProperty("webdriver.gecko.driver", testProperties.getProperty("webdriver.gecko.driver"));
+                    driver = new FirefoxDriver();
+                    break;
+            }
             driver.manage().window().maximize();
             driver.get(testProperties.getProperty("url"));
             driver .manage().timeouts().implicitlyWait(Integer.parseInt(testProperties.getProperty("timeout.global")), TimeUnit.SECONDS);
-            // driver .manage().timeouts().pageLoadTimeout(Integer.parseInt(testProperties.getProperty("timeout.pageLoad")), TimeUnit.SECONDS);
         }
 
 
